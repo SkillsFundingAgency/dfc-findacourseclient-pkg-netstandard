a5bdae186418f1a799cfb2ce3c27a82f0341dc13
gurney <Gurney@as-tech.co.uk>
2021-04-12 12:40:12 +0100

    +semver: minor

--- DFC.FindACourseClient.IntegrationTests/CourseSearchServiceTests.cs
+++ DFC.FindACourseClient.IntegrationTests/CourseSearchServiceTests.cs
@@ -107,6 +107,7 @@ namespace DFC.FindACourseClient.IntegrationTests
         {
             var courseSearchRequest = new CUIModels.CourseSearchProperties
             {
+                //Location for Birmingham
                 Filters = new CUIModels.CourseSearchFilters { SearchTerm = "Maths", Longitude = -1.877556, Latitude = 52.468725, Distance = 10, DistanceSpecified = true},
             };
 
