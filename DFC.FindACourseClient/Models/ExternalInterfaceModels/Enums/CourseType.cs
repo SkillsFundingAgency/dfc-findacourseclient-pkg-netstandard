2d5a658d4bd230a6ddfb74949230a3ac79d4b796
Pradeep Kumar Voorukonda <PradeepKumar.VOORUKONDA@EDUCATION.GOV.UK>
2024-02-28 11:46:09 +0000

    All CourseType option added

--- DFC.FindACourseClient/Models/ExternalInterfaceModels/Enums/CourseType.cs
+++ DFC.FindACourseClient/Models/ExternalInterfaceModels/Enums/CourseType.cs
@@ -7,22 +7,24 @@ namespace DFC.FindACourseClient
 {
     public enum CourseType
     {
-        [Display(Name = "Essential Skills", Order = 1)]
+        [Display(Name = "All", Order = 1)]
+        All,
+        [Display(Name = "Essential Skills", Order = 2)]
         EssentialSkills,
 
-        [Display(Name = "T-levels", Order = 2)]
+        [Display(Name = "T-levels", Order = 3)]
         TLevels,
 
-        [Display(Name = "HTQs", Order = 3)]
+        [Display(Name = "HTQs", Order = 4)]
         HTQs,
 
-        [Display(Name = "Free courses for Jobs", Order = 4)]
+        [Display(Name = "Free courses for Jobs", Order = 5)]
         FreeCoursesForJobs,
 
-        [Display(Name = "Multiply", Order = 5)]
+        [Display(Name = "Multiply", Order = 6)]
         Multiply,
 
-        [Display(Name = "Skills Bootcamps", Order = 5)]
+        [Display(Name = "Skills Bootcamps", Order = 7)]
         SkillsBootcamps,
     }
 }
